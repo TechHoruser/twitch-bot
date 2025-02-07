f0e4847847d1fd30657fd25d3df2dd1c9bdd94fb
Francisco Javier Ponce Rosales <franciscojavier.poncerosales@gmail.com>
2025-02-07 21:22:23 +0100

    ✨ feat: enhance overload handling and queue management in API and component

--- web/app/api/overload/route.js
+++ web/app/api/overload/route.js
@@ -1,22 +1,73 @@
 import fs from 'fs';
 
-const filePath = '/data/overload.json';
+const overloadCenterFilePath = '/data/overload-center.json';
+const overloadQueueFilePath = '/data/queue.json';
 
 export async function GET() {
   let isControllerClosed = false;
-  let lastData = null;
+  let lastPayload = {};
+  let lastQueue = [];
 
   const stream = new ReadableStream({
     start(controller) {
+      const checkCenterOverload = (payload) => {
+        if (JSON.stringify(payload) === JSON.stringify(lastPayload)) return;
+        controller.enqueue(`event: newOverload\ndata: ${JSON.stringify(payload)}\n\n`);
+      };
+
+      const checkQueueOverload = (queue) => {
+        if (lastQueue.length === 0) {
+          for (const item of queue) {
+            controller.enqueue(`event: newQueueElement\ndata: ${JSON.stringify(item)}\n\n`);
+          }
+          return;
+        }
+
+        if (queue.length === 0) {
+          for (const item of lastQueue) {
+            controller.enqueue(`event: dropQueueElement\ndata: ${item.uuid}\n\n`);
+          }
+          return;
+        }
+
+        let breakIndex = 0;
+        for (let i = 0; i < lastQueue.length; i++) {
+          const item = lastQueue[i];
+          const found = item.uuid === queue[0]?.uuid;
+          if (!found) {
+            controller.enqueue(`event: dropQueueElement\ndata: ${item.uuid}\n\n`);
+          } else {
+            breakIndex = i;
+            break;
+          }
+        }
+
+        for (let i = 0; i < queue.length; i++) {
+          const item = queue[i];
+          const isSimilar = item.uuid === lastQueue[breakIndex + i]?.uuid;
+          if (!isSimilar) {
+            do {
+              const item = queue[i];
+              controller.enqueue(`event: newQueueElement\ndata: ${JSON.stringify(item)}\n\n`);
+              i++;
+            } while (i < queue.length);
+            break;
+          }
+        }
+      }
+
       const sendChange = () => {
-        if (isControllerClosed) return;
-        
         try {
-          const data = fs.readFileSync(filePath, 'utf8');
-          const payload = JSON.parse(data);
-          if (JSON.stringify(payload) === JSON.stringify(lastData)) return;
-          lastData = payload;
-          controller.enqueue(`event: newOverload\ndata: ${JSON.stringify(payload)}\n\n`);
+          if (isControllerClosed) return;
+
+          const center = JSON.parse(fs.readFileSync(overloadCenterFilePath, 'utf8'));
+          checkCenterOverload(center);
+          lastPayload = center;
+
+          const queue = JSON.parse(fs.readFileSync(overloadQueueFilePath, 'utf8'));
+          checkQueueOverload(queue);
+          lastQueue = queue;
+
         } catch (error) {
           if (!isControllerClosed) {
             controller.enqueue(`event: error\ndata: ${error}\n\n`);
--- web/src/components/overloads/QueueOfPawns.jsx
+++ web/src/components/overloads/QueueOfPawns.jsx
@@ -1,49 +1,44 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 
-const toLoad = [
-  'bitsman',
-  'jadcgamer',
-  'Genevieve',
-  'Alfred',
-  'Michaela',
-  'Chris',
-  'Scout',
-  'Hunter',
-  'Penelope',
-  'Amir',
-]
+const Pawn = ({ position, name }) => (
+  <div
+    className="m-2 animate-slideLeft w-12 h-16 flex flex-col items-center justify-between"
+    style={{
+      animation: `slideLeft 1s ease-out forwards`,
+      animationDelay: `${position * 0.2}s`
+    }}
+  >
+    <img
+      src={`/images/pawn-queue.png`}
+      alt={name}
+    />
+    <p>{name}</p>
+  </div>
+);
 
-export const QueueOfPawns = ({
-  // data,
-}) => {
+export const QueueOfPawns = () => {
   const [data, setData] = useState([]);
-  const indexRef = useRef(0);
 
   useEffect(() => {
-    setTimeout(() => {
-      setData(prev => [...prev, toLoad[indexRef.current]]);
-      indexRef.current = indexRef.current + 1;
-    }, 2000);
-  }, [data]);
+    const evtSource = new EventSource('/api/overload');
+    evtSource.addEventListener('newQueueElement', (event) => {
+      const payload = JSON.parse(event.data);
+      setData(prev => [...prev, payload]);
+    });
+    evtSource.addEventListener('dropQueueElement', (event) => {
+      const uuid = event.data;
+      setData(prev => prev.filter(item => item.uuid !== uuid));
+    });
+    return () => {
+      evtSource.close();
+    };
+  }, []);
 
   return (
     <div className="w-full overflow-hidden">
       <div className="flex flex-row items-center justify-start">
-        {data.map((name, i) => (
-          <div 
-            key={i} 
-            className="m-2 animate-slideLeft w-12 h-16 flex flex-col items-center justify-between"
-            style={{
-              animation: `slideLeft 1s ease-out forwards`,
-              animationDelay: `${i * 0.2}s`
-            }}
-          >
-            <img
-              src={`/images/pawn-queue.png`}
-              alt={name}
-            />
-            <p>{name}</p>
-          </div>
+        {data.map((item, i) => (
+          <Pawn key={i} position={i} name={item.chesscom} />
         ))}
       </div>
     </div>
