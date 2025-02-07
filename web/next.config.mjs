69c73055595c630776b62aa8d60110dd2f41517b
Francisco Javier Ponce Rosales <franciscojavier.poncerosales@gmail.com>
2025-02-07 21:22:12 +0100

    ✨ chore: remove experimental configuration from Next.js config

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,16 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // experimental: {
-  //   turbo: {
-  //     loaders: {
-  //       // Configuración de loaders si es necesario
-  //     },
-  //     watch: {
-  //       usePolling: true,
-  //       interval: 300 // Intervalo de polling en milisegundos
-  //     }
-  //   },
-  // },
 };
 
 export default nextConfig;
